ed32253b4c6ef911f36f78f9fbfc2d056383b854
trainingjava <manidharkk@yahoomail.com>
2012-02-20 19:25:48 -0600

    First Commit

--- SocialWeb3/src/com/training/web/TestServlet.java
+++ SocialWeb3/src/com/training/web/TestServlet.java
@@ -20,7 +20,7 @@ public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
 	@EJB 
-	UserManagementBeanLocal ejb;
+	UserManagementBeanLocal ejb; //test
     /**
      * @see HttpServlet#HttpServlet()
      */
